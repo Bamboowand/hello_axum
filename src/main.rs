aeedde54c597a913250a5aac7fed3f20a0e0e48d
jacob_jheng <jacob@osensetech.com>
2023-08-29 18:46:12 +0800

    add file for test.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ async fn hello_world() -> &'static str {
 async fn main() {
     let app = Router::new().route("/", get(hello_world));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+    println!("listening on {}", addr);
 }
