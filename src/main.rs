780493e57dc548daf12ddf40598378be36abd3e8
jacob_jheng <jacob@osensetech.com>
2023-08-29 10:19:10 +0800

    refactor code.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
+async fn hello_world() -> &'static str {
+    "Hello world"
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world" }));
+    let app = Router::new().route("/", get(hello_world));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
